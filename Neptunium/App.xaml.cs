64dec66c0fe4e7da349979b3d7d43119b65901ca
Alex Mitchell <amrykid@gmail.com>
2015-05-25 14:20:03 -0500

    Initial commit for project Neptunium.

---
+++ Neptunium/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Crystal3;
+using Crystal3.Navigation;
+using Neptunium.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Application template is documented at http://go.microsoft.com/fwlink/?LinkId=402347&clcid=0x409
+
+namespace Neptunium
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : CrystalApplication
+    {
+        /// <summary>
+        /// Allows tracking page views, exceptions and other telemetry through the Microsoft Application Insights service.
+        /// </summary>
+        public static Microsoft.ApplicationInsights.TelemetryClient TelemetryClient;
+
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            TelemetryClient = new Microsoft.ApplicationInsights.TelemetryClient();
+        }
+
+        public override void OnFreshLaunch(LaunchActivatedEventArgs args)
+        {
+            NavigationManager.RootNavigationService.NavigateTo<AppShellViewModel>();
+        }
+    }
+}
