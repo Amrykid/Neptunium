13dba4f370430f079cefc9c0e14dcc475332d397
Alex Mitchell <amrykid@gmail.com>
2015-05-25 19:30:18 -0500

    Reorganizing and getting streaming to work. We have basic streaming but I may have to implement MSS to get shoutcast information from the audio "inband".

--- src/Neptunium/View/AppShellView.xaml.cs
+++ src/Neptunium/View/AppShellView.xaml.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Foundation.Metadata;
+using Windows.Media.Playback;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -26,7 +28,22 @@ namespace Neptunium.View
     {
         public AppShellView()
         {
-            //this.InitializeComponent();
+            this.InitializeComponent();
+
+
+            //AnimeNfo
+            BackgroundMediaPlayer.Current.SetMediaSource()
+            BackgroundMediaPlayer.Current.SetUriSource(new Uri("http://itori.animenfo.com:443/"));
+            BackgroundMediaPlayer.Current.PlaybackMediaMarkerReached += Current_PlaybackMediaMarkerReached;
+            BackgroundMediaPlayer.Current.Play();
+            
+
+            BackButton.Visibility = ApiInformation.IsTypePresent("Windows.Phone.UI.Input.HardwareButtons") ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        private void Current_PlaybackMediaMarkerReached(MediaPlayer sender, PlaybackMediaMarkerReachedEventArgs args)
+        {
+            
         }
 
         private void TogglePaneButton_Checked(object sender, RoutedEventArgs e)
