652e7b5badbd035f55755dc01593caf8b8020248
Amrykid <amrykid@gmail.com>
2015-08-03 15:26:52 -0500

    Updated .gitignore and added regular status text for phones.

--- src/Neptunium/ViewModel/AppShellViewModel.cs
+++ src/Neptunium/ViewModel/AppShellViewModel.cs
@@ -65,6 +65,8 @@ namespace Neptunium.ViewModel
                 if (BackgroundMediaPlayer.Current.CanPause)
                     BackgroundMediaPlayer.Current.Pause();
             }, x => { try { return BackgroundMediaPlayer.Current.CanPause; } catch (Exception) { return true; } });
+
+            WindowManager.GetStatusManagerForCurrentWindow().NormalStatusText = "Neptunium/Hanasu Alpha";
         }
 
         protected override async void OnNavigatedTo(object sender, CrystalNavigationEventArgs e)
