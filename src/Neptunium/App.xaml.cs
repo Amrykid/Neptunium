6c166022e3ca085c1a543fab138bdf13ff3bbe42
Alexander Mitchell-Robinson <amrykid@gmail.com>
2016-11-05 11:23:49 -0500

    Oops, forgot to recomment this.

--- src/Neptunium/App.xaml.cs
+++ src/Neptunium/App.xaml.cs
@@ -116,15 +116,16 @@ namespace Neptunium
         {
             base.OnConfigure();
 
-            if (CrystalApplication.GetDevicePlatform() == Crystal3.Core.Platform.Desktop)
-            {
-#if DEBUG
-                if (Gamepad.Gamepads.Count > 0)
-                {
-                    this.Options.OverridePlatform(Crystal3.Core.Platform.Xbox);
-                }
-#endif
-            }
+//            if (CrystalApplication.GetDevicePlatform() == Crystal3.Core.Platform.Desktop)
+//            {
+//#if DEBUG
+//                //however over .Count once with a breakpoint once to force system to recognize xbox one controller.
+//                if (Gamepad.Gamepads.Count > 0)
+//                {
+//                    this.Options.OverridePlatform(Crystal3.Core.Platform.Xbox);
+//                }
+//#endif
+//            }
 
             this.Options.HandleSystemBackNavigation = CrystalApplication.GetDevicePlatform() != Crystal3.Core.Platform.Xbox;
         }
