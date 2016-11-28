115e8ab04ed56fe44e69de08e9a297c1d75aa4b2
Alexander Mitchell-Robinson <amrykid@gmail.com>
2016-11-27 19:07:40 -0600

    Switched the now playing pages to the new glass control.

--- src/Neptunium/View/NowPlayingView.xaml.cs
+++ src/Neptunium/View/NowPlayingView.xaml.cs
@@ -32,18 +32,16 @@ namespace Neptunium.View
     [Crystal3.Navigation.NavigationViewModel(typeof(NowPlayingViewViewModel), NavigationViewSupportedPlatform.Desktop | NavigationViewSupportedPlatform.Mobile)]
     public sealed partial class NowPlayingView : Page
     {
-        private SpriteVisual glassVisual;
         private NowPlayingViewViewModel viewModel = null;
 
         public NowPlayingView()
         {
             this.InitializeComponent();
-            InitializeFrostedGlass(GlassHost);
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-            ElementCompositionPreview.SetElementChildVisual(GlassHost, null); //turn off the glass
+            GlassPanel.TurnOffGlass(); //turn off the glass
         }
 
         private async void ViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
@@ -58,61 +56,15 @@ namespace Neptunium.View
 
                 if (string.IsNullOrWhiteSpace(imgSrc))
                 {
-                    ElementCompositionPreview.SetElementChildVisual(GlassHost, null); //turn off the glass
+                    GlassPanel.TurnOffGlass(); //turn off the glass
                 }
                 else
                 {
                     //todo check if url doesn't 404
 
-                    ElementCompositionPreview.SetElementChildVisual(GlassHost, glassVisual); //turn on glass
+                    GlassPanel.TurnOnGlass(); //turn on glass
                 }
             }
-        }
-
-        private void InitializeFrostedGlass(UIElement glassHost)
-        {
-            //https://msdn.microsoft.com/en-us/windows/uwp/graphics/using-the-visual-layer-with-xaml
-
-            Visual hostVisual = ElementCompositionPreview.GetElementVisual(glassHost);
-            Compositor compositor = hostVisual.Compositor;
-
-            // Create a glass effect, requires Win2D NuGet package
-            var glassEffect = new GaussianBlurEffect
-            {
-                BlurAmount = 7.0f, //original value: 15.0f
-                BorderMode = EffectBorderMode.Hard,
-                Source = new ArithmeticCompositeEffect
-                {
-                    MultiplyAmount = 0,
-                    Source1Amount = 0.5f,
-                    Source2Amount = 0.5f,
-                    Source1 = new CompositionEffectSourceParameter("backdropBrush"),
-                    Source2 = new ColorSourceEffect
-                    {
-                        Color = Color.FromArgb(255, 245, 245, 245)
-                    }
-                }
-            };
-
-            //  Create an instance of the effect and set its source to a CompositionBackdropBrush
-            var effectFactory = compositor.CreateEffectFactory(glassEffect);
-            var backdropBrush = compositor.CreateBackdropBrush();
-            var effectBrush = effectFactory.CreateBrush();
-
-            effectBrush.SetSourceParameter("backdropBrush", backdropBrush);
-
-            // Create a Visual to contain the frosted glass effect
-            glassVisual = compositor.CreateSpriteVisual();
-            glassVisual.Brush = effectBrush;
-
-            // Add the blur as a child of the host in the visual tree
-            ElementCompositionPreview.SetElementChildVisual(glassHost, glassVisual);
-
-            // Make sure size of glass host and glass visual always stay in sync
-            var bindSizeAnimation = compositor.CreateExpressionAnimation("hostVisual.Size");
-            bindSizeAnimation.SetReferenceParameter("hostVisual", hostVisual);
-
-            glassVisual.StartAnimation("Size", bindSizeAnimation);
         }
 
         private void Page_DataContextChanged(FrameworkElement sender, DataContextChangedEventArgs args)
@@ -130,8 +82,6 @@ namespace Neptunium.View
         {
             if (viewModel != null)
                 viewModel.PropertyChanged -= ViewModel_PropertyChanged;
-
-            glassVisual.Dispose();
         }
     }
 }
