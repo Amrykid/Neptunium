0478154c64be9d629a1dcd974e3d8b68eee55a93
Alex Mitchell <amrykid@gmail.com>
2017-10-07 22:01:40 -0500

    More tweaks to the JPopAsia fetching code.

--- src/Neptunium/Core/Media/Metadata/JPopAsiaFetcher.cs
+++ src/Neptunium/Core/Media/Metadata/JPopAsiaFetcher.cs
@@ -20,7 +20,7 @@ namespace Neptunium.Core.Media.Metadata
             HttpClient http = new HttpClient();
             HttpResponseMessage httpResponse = null;
 
-            Uri directUri = new Uri(string.Format("http://www.jpopasia.com/{0}/", Uri.UnescapeDataString(artistName))); //try and use a direct url. this works for artists like "Superfly" or "Perfume"
+            Uri directUri = new Uri(string.Format("http://www.jpopasia.com/{0}/", Uri.EscapeUriString(artistName))); //try and use a direct url. this works for artists like "Superfly" or "Perfume"
 
             httpResponse = await http.GetAsync(directUri);
             if (httpResponse.IsSuccessStatusCode)
@@ -109,6 +109,8 @@ namespace Neptunium.Core.Media.Metadata
             {
                 httpResponse.Dispose();
             }
+
+            //todo cache
 
             return result;
         }
