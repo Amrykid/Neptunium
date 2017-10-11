6b8de366335c4072d496cf227606cc1dc249bc95
Alex Mitchell <amrykid@gmail.com>
2017-10-10 21:48:12 -0500

    One more minor edit to the JPAFetcher for tonight.

--- src/Neptunium/Core/Media/Metadata/JPopAsiaFetcher.cs
+++ src/Neptunium/Core/Media/Metadata/JPopAsiaFetcher.cs
@@ -24,7 +24,8 @@ namespace Neptunium.Core.Media.Metadata
                 string.Format("http://www.jpopasia.com/{0}/", 
                     Uri.EscapeUriString(
                         artistName.Replace(" ","")
-                        .Replace("!","")))); //try and use a direct url. this works for artists like "Superfly" or "Perfume"
+                        .Replace("!","")
+                        .Replace("*","")))); //try and use a direct url. this works for artists like "Superfly" or "Perfume"
 
             httpResponse = await http.GetAsync(directUri);
             if (httpResponse.IsSuccessStatusCode)
