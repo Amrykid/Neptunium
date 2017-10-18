262291263748c44d698e44d8b7b25034ee95cd7f
Alex Mitchell <amrykid@gmail.com>
2017-10-17 20:57:53 -0500

    Constructor and input functions created for the song manager.

--- src/Neptunium/Core/Media/Songs/NepAppSongManager.cs
+++ src/Neptunium/Core/Media/Songs/NepAppSongManager.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Neptunium.Core.Media.Metadata;
+using Neptunium.Core.Stations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using static Neptunium.NepApp;
 
@@ -9,6 +12,14 @@ namespace Neptunium.Core.Media.Songs
 {
     public class NepAppSongManager: INepAppFunctionManager
     {
+        internal NepAppSongManager()
+        {
 
+        }
+
+        internal async void HandleMetadata(SongMetadata songMetadata, StationStream currentStrean)
+        {
+            
+        }
     }
 }
