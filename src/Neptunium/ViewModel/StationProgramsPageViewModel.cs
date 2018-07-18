cea13a35e5a18eb8c49f3e014f13f76fab301835
Amrykid <amrykid@gmail.com>
2018-07-18 18:52:09 -0500

    Corrected the order of days on the schedule/program page.

--- src/Neptunium/ViewModel/StationProgramsPageViewModel.cs
+++ src/Neptunium/ViewModel/StationProgramsPageViewModel.cs
@@ -77,13 +77,38 @@ namespace Neptunium.ViewModel
             var collectionViewSource = new CollectionViewSource();
             collectionViewSource.Source = items
                 .OrderBy(x => x.TimeLocal.Hour)
-                .OrderBy(x => (int)x.TimeLocal.DayOfWeek)
-                .GroupBy(x => x.Day);
+                .GroupBy(x => x.Day)
+                .OrderBy(x => x.Key, new DayComparer());
             collectionViewSource.IsSourceGrouped = true;
             SortedScheduleItems = collectionViewSource;
 
             RaisePropertyChanged(nameof(ScheduleItems));
             RaisePropertyChanged(nameof(SortedScheduleItems));
+        }
+
+        private class DayComparer : IComparer<string>
+        {
+            private int GetDayNumber(string day)
+            {
+                switch(day.ToLower())
+                {
+                    case "sunday": return 0;
+                    case "monday": return 1;
+                    case "tuesday": return 2;
+                    case "wednesday": return 3;
+                    case "thursday": return 4;
+                    case "friday": return 5;
+                    case "saturday": return 6;
+                }
+
+                return 0;
+            }
+            public int Compare(string x, string y)
+            {
+                if (GetDayNumber(x) > GetDayNumber(y)) return 1;
+                else if (GetDayNumber(x) < GetDayNumber(y)) return -1;
+                else return 0;
+            }
         }
     }
 }
