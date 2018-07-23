a4fe1bdd3e1694b55e5c6094783b3512295b50bf
Amrykid <amrykid@gmail.com>
2018-07-23 09:15:42 -0500

    Updates to ensure integration with data saver.

--- src/Neptunium/Core/NepAppNetworkManager.cs
+++ src/Neptunium/Core/NepAppNetworkManager.cs
@@ -1,15 +1,17 @@
-﻿using Windows.Networking.Connectivity;
+﻿using System;
+using Windows.Networking.Connectivity;
 using static Neptunium.NepApp;
 
 namespace Neptunium
 {
-    public class NepAppNetworkManager: INepAppFunctionManager
+    public class NepAppNetworkManager : INepAppFunctionManager
     {
         public NepAppNetworkManager()
         {
             NetworkInformation.NetworkStatusChanged += NetworkInformation_NetworkStatusChanged;
 
             IsConnected = IsInternetConnected();
+            UpdateNetworkUtilizationBehavior();
         }
 
         private bool IsInternetConnected()
@@ -28,10 +30,49 @@ namespace Neptunium
 
             if (oldStatus != newStatus)
             {
-                //todo fire an event stating that the connection changed.
+                IsConnectedChanged?.Invoke(this, EventArgs.Empty);
+                UpdateNetworkUtilizationBehavior();
             }
         }
 
+        private void UpdateNetworkUtilizationBehavior()
+        {
+            //https://docs.microsoft.com/en-us/previous-versions/windows/apps/jj835821(v=win.10)
+            ConnectionProfile connections = NetworkInformation.GetInternetConnectionProfile();
+
+            if (connections != null)
+            {
+                var cost = connections.GetConnectionCost();
+                var dataPlan = connections.GetDataPlanStatus();
+
+                if (cost.NetworkCostType == NetworkCostType.Unrestricted || cost.NetworkCostType == NetworkCostType.Unknown)
+                {
+                    NetworkUtilizationBehavior = NetworkDeterminedAppBehaviorStyle.Normal;
+                }
+                else if (cost.NetworkCostType == NetworkCostType.Fixed || cost.NetworkCostType == NetworkCostType.Variable)
+                {
+                    if (!cost.Roaming && !cost.OverDataLimit)
+                    {
+                        NetworkUtilizationBehavior = NetworkDeterminedAppBehaviorStyle.Conservative;
+                    }
+                    else
+                    {
+                        NetworkUtilizationBehavior = NetworkDeterminedAppBehaviorStyle.OptIn;
+                    }
+                }
+            }
+        }
+
+        public event EventHandler IsConnectedChanged;
         public bool IsConnected { get; private set; }
+
+        public NetworkDeterminedAppBehaviorStyle NetworkUtilizationBehavior { get; private set; }
+
+        public enum NetworkDeterminedAppBehaviorStyle
+        {
+            Normal,
+            Conservative,
+            OptIn
+        }
     }
 }
