e91f1b16d409a18a4f7941e7a5095ffbdd6d3653
Amrykid <amrykid@gmail.com>
2018-08-10 10:17:09 -0500

    Working on the server remote page a bit.

--- src/Neptunium/ViewModel/ServerRemotePageViewModel.cs
+++ src/Neptunium/ViewModel/ServerRemotePageViewModel.cs
@@ -1,17 +1,43 @@
 ﻿using Crystal3.Model;
 using Crystal3.Navigation;
+using Crystal3.UI.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Neptunium.ViewModel
 {
-    public class ServerRemotePageViewModel: ViewModelBase
+    public class ServerRemotePageViewModel : ViewModelBase
     {
+        private Neptunium.Core.NepAppServerFrontEndManager.NepAppServerClient serverClient = null;
         protected override void OnNavigatedTo(object sender, CrystalNavigationEventArgs e)
         {
+            Neptunium.Core.NepAppServerFrontEndManager.NepAppServerClient serverClient = new Core.NepAppServerFrontEndManager.NepAppServerClient();
+
+            ConnectCommand = new ManualRelayCommand(async parameter =>
+            {
+                try
+                {
+                    await serverClient.TryConnectAsync(IPAddress.Parse((string)parameter);
+                    IsConnected = true;
+                }
+                catch (Exception ex)
+                {
+                    IsConnected = false;
+                }
+            });
+
+            DisconnectCommand = new ManualRelayCommand(parameter =>
+            {
+
+            });
+
+            ConnectCommand.SetCanExecute(!IsConnected);
+            DisconnectCommand.SetCanExecute(IsConnected);
+
             base.OnNavigatedTo(sender, e);
         }
 
@@ -19,5 +45,21 @@ namespace Neptunium.ViewModel
         {
             base.OnNavigatedFrom(sender, e);
         }
+
+        public bool IsConnected
+        {
+            get { return GetPropertyValue<bool>(); }
+            private set
+            {
+                SetPropertyValue<bool>(value: value);
+
+                ConnectCommand.SetCanExecute(!value);
+                DisconnectCommand.SetCanExecute(value);
+            }
+        }
+
+        public ManualRelayCommand ConnectCommand { get; private set; }
+
+        public ManualRelayCommand DisconnectCommand { get; private set; }
     }
 }
