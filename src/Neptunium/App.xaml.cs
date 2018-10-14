5c926c653883bda306833dbe771d676fd0e36f65
Amrykid <amrykid@gmail.com>
2018-10-14 09:59:46 -0500

    Disabled view model caching. Instead, remove an internal reflection call in Crystal.

--- src/Neptunium/App.xaml.cs
+++ src/Neptunium/App.xaml.cs
@@ -187,7 +187,6 @@ namespace Neptunium
             }
 
             this.Options.HandleSystemBackNavigation = true;
-            this.Options.EnableViewModelCaching = true;
         }
 
         protected override async Task OnApplicationInitializedAsync()
